52d131f192e00c216fdc87fa5ce9eb584c289007
Sozla.uz <nortojiyev642@gmail.com>
2024-06-13 09:26:20 +0500

    writing dockstring for models

--- app_users/models.py
+++ app_users/models.py
@@ -9,6 +9,24 @@ from django.utils import timezone
 
 
 class User(AbstractUser):
+    """
+    Custom User model extending AbstractUser.
+
+    Fields:
+    - first_name (CharField): User's first name with max length of 100.
+    - last_name (CharField): User's last name with max length of 100.
+    - username (CharField): Username with max length of 100, can be blank.
+    - email (CharField): User's email, must be unique.
+    - profile_image (ImageField): Profile image, default is 'images/profile_image.png'.
+    - is_staff (BooleanField): Indicates if the user is a staff member.
+    - is_superuser (BooleanField): Indicates if the user is a superuser.
+    - is_active (BooleanField): Indicates if the user is active.
+
+    Methods:
+    - get_full_name: Returns the full name of the user.
+    - __str__: Returns a string representation of the user.
+    """
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     username = models.CharField(max_length=100, blank=True)
@@ -24,13 +42,31 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
 
     def get_full_name(self):
+        """Returns the full name of the user."""
         return f"{self.first_name} {self.last_name}"
 
     def __str__(self):
+        """Returns a string representation of the user."""
         return f'{self.first_name} {self.last_name}'
 
 
 class Kirim(models.Model):
+    """
+    Model representing income (Kirim).
+
+    Fields:
+    - user (ForeignKey): Reference to the User model.
+    - summa (DecimalField): Amount of income.
+    - qayerdan (CharField): Source of the income.
+    - sana (DateTimeField): Date and time of the income.
+    - tolov_turi (CharField): Payment type, either 'cash' or 'card'.
+    - ish_haqqi_turi (CharField): Source type, either 'monthly', 'advance', or 'daily'.
+    - status (str): Status of the transaction, fixed as 'kirim'.
+
+    Methods:
+    - __str__: Returns a string representation of the Kirim instance.
+    """
+
     CASH = 'cash'
     CARD = 'card'
     PAYMENT_CHOICES = [
@@ -54,11 +90,29 @@ class Kirim(models.Model):
     tolov_turi = models.CharField(max_length=10, choices=PAYMENT_CHOICES, default=CASH)
     ish_haqqi_turi = models.CharField(max_length=10, choices=SOURCE_TYPE_CHOICES, default=MONTHLY)
     status = 'kirim'
+
     def __str__(self):
+        """Returns a string representation of the Kirim instance."""
         return f"{self.summa} - {self.qayerdan} UZS"
 
 
 class Chiqim(models.Model):
+    """
+    Model representing expenses (Chiqim).
+
+    Fields:
+    - user (ForeignKey): Reference to the User model.
+    - summa (DecimalField): Amount of expense.
+    - qayerga (CharField): Destination of the expense.
+    - tolov_turi (CharField): Payment type, either 'NAQT' or 'KARTA'.
+    - chiqim_turi (CharField): Type of expense.
+    - sana (DateTimeField): Date and time of the expense.
+    - status (str): Status of the transaction, fixed as 'chiqim'.
+
+    Methods:
+    - __str__: Returns a string representation of the Chiqim instance.
+    """
+
     NAQT = 'NAQT'
     KARTA = 'KARTA'
 
@@ -74,5 +128,7 @@ class Chiqim(models.Model):
     chiqim_turi = models.CharField(max_length=50, null=True, blank=True)
     sana = models.DateTimeField(default=timezone.now)
     status = 'chiqim'
+
     def __str__(self):
+        """Returns a string representation of the Chiqim instance."""
         return f"{self.summa} UZS to {self.qayerga}"
